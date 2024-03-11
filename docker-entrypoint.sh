605f489aa8138196dcd48272c5dd498e2b82c4f4
UnpixeltGuard <118729006+UnpixeltGuard@users.noreply.github.com>
2024-03-11 13:50:37 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,10 +4,10 @@ echo "Starting JMusicBot made by ${bold}jagrosh${bold} (https://github.com/jagro
 JMusicBot version: $VERSION, Dockerized on: $DATE"
 echo ""
 
-FILE=/app/config/config.txt
+FILE=/app/config.txt
 if [ ! -f "$FILE" ]; then
     echo "No config.txt found, downloading example config from
-    https://github.com/jagrosh/MusicBot/releases/download/0.2.9/config.txt"
+    https://raw.githubusercontent.com/UnpixeltGuard/pixmusic/main/example-config"
     
     curl -L https://raw.githubusercontent.com/UnpixeltGuard/pixmusic/main/example-config -o /app/config.txt
     echo ""
